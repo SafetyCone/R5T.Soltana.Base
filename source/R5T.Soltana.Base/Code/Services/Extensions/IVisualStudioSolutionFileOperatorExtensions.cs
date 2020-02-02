e20b434f8bcc7dc730fa3d0efe76c585b65f630d
MinexAutomation <minex.automation@gmail.com>
2020-02-02 01:26:55 -0800

    Added SolutionFileSite-related IVisualStudioSolutionFileOperator extensions.

---
+++ source/R5T.Soltana.Base/Code/Services/Extensions/IVisualStudioSolutionFileOperatorExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+using R5T.Cambridge.Types;
+
+
+namespace R5T.Soltana
+{
+    public static class IVisualStudioSolutionFileOperatorExtensions
+    {
+        public static void AddProjectFile(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator, SolutionFileSite solutionFileSite, string projectFilePath)
+        {
+            visualStudioSolutionFileOperator.AddProjectFile(solutionFileSite.SolutionFile, solutionFileSite.Path, projectFilePath);
+        }
+
+        public static void RemoveProjectFile(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator, SolutionFileSite solutionFileSite, string projectFilePath)
+        {
+            visualStudioSolutionFileOperator.RemoveProjectFile(solutionFileSite.SolutionFile, solutionFileSite.Path, projectFilePath);
+        }
+
+        public static IEnumerable<string> ListProjectFiles(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator, SolutionFileSite solutionFileSite)
+        {
+            var projectFilePaths = visualStudioSolutionFileOperator.ListProjectFiles(solutionFileSite.SolutionFile, solutionFileSite.Path);
+            return projectFilePaths
+        }
+    }
+}
