f4f238cdc4958cf81e044f73f60f91244bd6c816
MinexAutomation <minex.automation@gmail.com>
2020-02-07 00:29:51 -0800

    Improved IVisualStudioSolutionFileOperator service definition. Improved documentation.

--- source/R5T.Soltana.Base/Code/Services/Definitions/IVisualStudioSolutionFileOperator.cs
+++ source/R5T.Soltana.Base/Code/Services/Definitions/IVisualStudioSolutionFileOperator.cs
@@ -8,21 +8,27 @@ namespace R5T.Soltana
 {
     /// <summary>
     /// A service for operating on (in-memory) <see cref="SolutionFile"/> instances.
+    /// The <see cref="R5T.Soltana.IVisualStudioSolutionFileOperator"/> service:
+    /// 1) Performs file path operations like adding or removing a project file.
+    /// 2) Lists project-references, filtering out solution folders.
+    /// 3) Creates new <see cref="SolutionFile"/> instances.
     /// </summary>
     /// <remarks>
     /// Throughout, a solution file path is required to turn relative paths in the solution file into absolute project file paths.
     /// </remarks>
     public interface IVisualStudioSolutionFileOperator
     {
-        SolutionFile CreateNew();
+        SolutionFile CreateNewSolutionFile();
 
-        void AddProjectFile(SolutionFile solutionFile, string solutionFilePath, string projectFilePath);
+        void AddProjectFile(SolutionFile solutionFile, string solutionFilePath, string projectFilePath, Guid projectTypeGuid);
 
-        void RemoveProjectFile(SolutionFile solutionFile, string solutionFilePath, string projectFilePath);
+        bool HasProjectFile(SolutionFile solutionFile, string solutionFilePath, string projectFilePath);
+
+        bool RemoveProjectFile(SolutionFile solutionFile, string solutionFilePath, string projectFilePath);
 
         /// <summary>
-        /// Lists project file paths in the solution.
+        /// Lists solution project file references, NOT including any solution folders.
         /// </summary>
-        IEnumerable<string> ListProjectFiles(SolutionFile solutionFile, string solutionFilePath);
+        IEnumerable<SolutionFileProjectFileReference> ListProjectFileReferences(SolutionFile solutionFile, string solutionFilePath);
     }
 }
