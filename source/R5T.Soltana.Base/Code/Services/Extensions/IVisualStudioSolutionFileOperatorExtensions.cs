90bb8206b08ee259c2d1ca64245e17d1ac24efdd
MinexAutomation <minex.automation@gmail.com>
2020-02-07 00:30:24 -0800

    Improved IVisualStudioSolutionFileOperator extensions.

--- source/R5T.Soltana.Base/Code/Services/Extensions/IVisualStudioSolutionFileOperatorExtensions.cs
+++ source/R5T.Soltana.Base/Code/Services/Extensions/IVisualStudioSolutionFileOperatorExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 using R5T.Cambridge.Types;
 
@@ -8,20 +9,71 @@ namespace R5T.Soltana
 {
     public static class IVisualStudioSolutionFileOperatorExtensions
     {
-        public static void AddProjectFile(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator, SolutionFileSite solutionFileSite, string projectFilePath)
+        public static SolutionFileSite CreateNewSolutionFileSite(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator, string solutionFilePath)
         {
-            visualStudioSolutionFileOperator.AddProjectFile(solutionFileSite.SolutionFile, solutionFileSite.Path, projectFilePath);
+            var solutionFile = visualStudioSolutionFileOperator.CreateNewSolutionFile();
+
+            var solutionFileSite = new SolutionFileSite()
+            {
+                SolutionFile = solutionFile,
+                SolutionFilePath = solutionFilePath,
+            };
+            return solutionFileSite;
         }
 
-        public static void RemoveProjectFile(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator, SolutionFileSite solutionFileSite, string projectFilePath)
+        public static void AddProjectFile(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator, SolutionFileSite solutionFileSite, string projectFilePath, Guid projectTypeGuid)
         {
-            visualStudioSolutionFileOperator.RemoveProjectFile(solutionFileSite.SolutionFile, solutionFileSite.Path, projectFilePath);
+            visualStudioSolutionFileOperator.AddProjectFile(solutionFileSite.SolutionFile, solutionFileSite.SolutionFilePath, projectFilePath, projectTypeGuid);
         }
 
-        public static IEnumerable<string> ListProjectFiles(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator, SolutionFileSite solutionFileSite)
+        public static bool HasProjectFile(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator, SolutionFileSite solutionFileSite, string projectFilePath)
         {
-            var projectFilePaths = visualStudioSolutionFileOperator.ListProjectFiles(solutionFileSite.SolutionFile, solutionFileSite.Path);
-            return projectFilePaths
+            var hasProjectFile = visualStudioSolutionFileOperator.HasProjectFile(solutionFileSite.SolutionFile, solutionFileSite.SolutionFilePath, projectFilePath);
+            return hasProjectFile;
         }
+
+        public static bool RemoveProjectFile(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator, SolutionFileSite solutionFileSite, string projectFilePath)
+        {
+            var removed = visualStudioSolutionFileOperator.RemoveProjectFile(solutionFileSite.SolutionFile, solutionFileSite.SolutionFilePath, projectFilePath);
+            return removed;
+        }
+
+        public static IEnumerable<SolutionFileProjectFileReference> ListProjectFileReferences(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator, SolutionFileSite solutionFileSite)
+        {
+            var projectFileSpecifications = visualStudioSolutionFileOperator.ListProjectFileReferences(solutionFileSite.SolutionFile, solutionFileSite.SolutionFilePath);
+            return projectFileSpecifications;
+        }
+
+        public static IEnumerable<string> ListProjectFileReferences(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator, SolutionFile solutionFile, string solutionFilePath)
+        {
+            var projectFileSpecifications = visualStudioSolutionFileOperator.ListProjectFileReferences(solutionFile, solutionFilePath);
+
+            var projectFilePaths = projectFileSpecifications.Select(x => x.ProjectFilePathValue);
+            return projectFilePaths;
+        }
+
+        public static IEnumerable<string> ListProjectFilePaths(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator, SolutionFileSite solutionFileSite)
+        {
+            var projectFilePaths = ListProjectFileReferences(visualStudioSolutionFileOperator, solutionFileSite.SolutionFile, solutionFileSite.SolutionFilePath);
+            return projectFilePaths;
+        }
+
+        public static SolutionFileProjectFileReference GetProjectFileSpecification(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator, SolutionFile solutionFile, string solutionFilePath, string projectFilePath)
+        {
+            var projectFileSpecifications = visualStudioSolutionFileOperator.ListProjectFileReferences(solutionFile, solutionFilePath);
+
+            var desiredProjectFileSpecification = projectFileSpecifications.Where(x => x.ProjectFilePathValue == projectFilePath).Single();
+            return desiredProjectFileSpecification;
+        }
+
+        public static SolutionFileProjectFileReference GetProjectFileSpecification(this IVisualStudioSolutionFileOperator visualStudioSolutionFileOperator, SolutionFileSite solutionFileSite, string projectFilePath)
+        {
+            var projectFileSpecification = visualStudioSolutionFileOperator.GetProjectFileSpecification(solutionFileSite.SolutionFile, solutionFileSite.SolutionFilePath, projectFilePath);
+            return projectFileSpecification;
+        }
+
+        // Include adding a solution folder?
+
+        // Add 
     }
 }
